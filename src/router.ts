6ea39ea0de12d3081368a6ca236124f3da9874f6
Zoe Viola <zvo@mediaan.com>
2024-03-17 17:12:05 +0100

    added map

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,12 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "./views/HomeView.vue";
 import BeersView from "./views/BeersView.vue";
 import { useDevice } from "./composables/device";
+import MapView from "./views/MapView.vue";
 
 const routes = [
   { path: "/TheBeerExperience24/", name: "home", component: HomeView },
   { path: "/TheBeerExperience24/beers", name: "beers", component: BeersView },
-  { path: "/TheBeerExperience24/map", name: "map", component: BeersView },
+  { path: "/TheBeerExperience24/map", name: "map", component: MapView },
   { path: "/TheBeerExperience24/food", name: "food", component: BeersView },
   {
     path: "/TheBeerExperience24/partners",
---
+++ src/views/MapView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <Map />
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  import Map from "../components/Map.vue";
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
+  
