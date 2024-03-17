953ab8e537599be49488d937da27cd5272167f9f
Zoe Viola <zvo@mediaan.com>
2024-03-17 17:54:09 +0100

    changed map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,30 +1,17 @@
 <template>
-    <div class="pdf-container">
-      <iframe :src="pdfUrl" class="pdf-iframe"></iframe>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref } from 'vue';
-  
-  const pdfUrl = ref('/TheBeerExperience24/assets/plan.pdf#toolbar=0');
-  </script>
-  
-  <style scoped>
-  .pdf-container {
-    width: 100%;
-    height: 100vh; /* Use the full viewport height */
-    overflow: auto; /* Enable scrolling if the content is larger than the container */
-  }
-  
-  .pdf-iframe {
-    width: 100%;
-    height: 100%;
-  }
-  
-  @media (max-width: 600px) {
-    .pdf-iframe {
-      height: 600px; /* Adjust the height for small screens */
-    }
-  }
-  </style>
+  <VuePdfEmbed annotation-layer text-layer :source="pdfSource" />
+</template>
+
+<script setup lang="ts">
+import VuePdfEmbed from "vue-pdf-embed";
+
+// essential styles
+import "vue-pdf-embed/dist/style/index.css";
+
+// optional styles
+import "vue-pdf-embed/dist/style/annotationLayer.css";
+import "vue-pdf-embed/dist/style/textLayer.css";
+
+// either URL, Base64, binary, or document proxy
+const pdfSource = "/TheBeerExperience24/assets/plan.pdf";
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/TheBeerExperience24/",
-  plugins: [vue()],
-  assetsInclude: ["**/*.jpg", "**/*.JPG"],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    base: "/TheBeerExperience24/",
+    plugins: [vue()],
+    assetsInclude: ["**/*.jpg", "**/*.JPG"],
+    define: {
+      "process.env": env,
+    },
+  })
+}
