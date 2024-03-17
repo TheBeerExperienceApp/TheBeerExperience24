ac1dc3acd1088d767c7a367dec4e4e7fa1647102
Zoe Viola <zvo@mediaan.com>
2024-03-17 18:35:07 +0100

    iframe

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,17 +1,10 @@
 <template>
-  <VuePdfEmbed annotation-layer text-layer :source="pdfSource" />
-</template>
-
-<script setup lang="ts">
-import VuePdfEmbed from "vue-pdf-embed";
-
-// essential styles
-import "vue-pdf-embed/dist/style/index.css";
-
-// optional styles
-import "vue-pdf-embed/dist/style/annotationLayer.css";
-import "vue-pdf-embed/dist/style/textLayer.css";
-
-// either URL, Base64, binary, or document proxy
-const pdfSource = "/TheBeerExperience24/assets/plan.pdf";
-</script>
+    <div>
+      <iframe :src="pdfSource" width="100%" height="600px"></iframe>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  // either URL, Base64, binary, or document proxy
+  const pdfSource = "/TheBeerExperience24/assets/plan.pdf#toolbar=0#zoom=50";
+  </script>
